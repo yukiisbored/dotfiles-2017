2d007fd0215bda420c8b580c0d7f395858bdfa8b
Muhammad Kaisar Arkhan <yukiisbored@disroot.org>
2017-11-26 21:27:33 +0700

    config.h: Launch GIMP floating

--- src/dwm/config.h
+++ src/dwm/config.h
@@ -25,6 +25,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+  { "Gimp", NULL, NULL, 0, 1, -1 }
 };
 
 /* layout(s) */
